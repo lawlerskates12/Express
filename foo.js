a11ec3ea080616a1b980648ca7b5e90169ec3c99
lawlerskates12 <lawlerskates12@lawl.lawl>
2016-09-07 13:30:58 -0700

    Foo

--- foo.js
+++ foo.js
@@ -1,84 +1,40 @@
-var express = require('express');
-var jwt = require('jsonwebtoken');
-var cookieParser = require('cookie-parser')
+'use strict';
 
-var port = 3000;
+var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 var app = express();
 
-var user = {
-  username: 'foo',
-  password: 'bar'
-};
+var routesArray = ['/login', '/auth', '/signup', '/email', '/chPassW', '/logout', '/snapshot'];
 
-var tokenSettings = {
-  alg: 'HS256'
-};
 
 app.disable('x-powered-by');
-app.use(cookieParser())
+app.use(routesArray, session({
+  genid: tokenGen,
+  name: 'auth_cookie',
+  secret: 'Nk8Y9b22n88QUtkR7uO3Bdgf274mlh68',
+  cookie: {
+    secure: true,
+    httpOnly: true
+  },  
+}));
 
-app.get('/', function(req, res) {
-  res.send('hey there');
+
+function tokenGen(req) {
+  const token = String(Math.floor(Math.random() * 1000000));
+  return token;
+}
+
+app.get('/', function(req, res){
+  res.send('Unauthenticated page');
 });
 
-app.get('/login', function(req, res) {
-  if (req.query.username == user.username &&
-      req.query.password == user.password) {
-    var token = jwt.sign(user, secret, {
-      expiresIn: '5m',
-      alg: 'HS256'
-    });
-    res.cookie('token', token, {});
-    res.send('you should have a cookie');
-  } else {
-    res.send('bad login');
-  }
+app.get('/login', function(req, res){
+  res.send('You are authenticated');
 });
 
-app.get('/test', function (req, res) {
-  if (req.cookies.token) {
-    jwt.verify(req.cookies.token, 'test4',
-	       {
-		 ignoreExpiration: true,
-		 algorithms: ['HS256']
-	       }, function (err, token) {
-		 res.json(token);
-	       });
-  } else {
-    res.send('no token');
-  }
-});
 
-app.get('/test2', function (req, res) {
-  if (req.cookies.token) {
-    jwt.verify(req.cookies.token, 'test4',
-	       {
-		 ignoreNotBefore: true,
-		 algorithms: ['HS256']
-	       }, function (err, token) {
-		 res.json(token);
-	       });
-  } else {
-    res.send('no token');
-  }
-});
+app.listen(3000);
+console.log("Server running on port 3000");
 
-app.get('/test3', function (req, res) {
-  if (req.cookies.token) {
-    jwt.verify(req.cookies.token, 'test4',
-	       {
-		 ignoreNotBefore: true,
-		 ignoreExpiration: true,
-		 algorithms: ['HS256']
-	       }, function (err, token) {
-		 res.json(token);
-	       });
-  } else {
-    res.send('no token');
-  }
-});
-
-app.listen(port, () => {
-  console.log("i'm listening...");
-});
-
+x
